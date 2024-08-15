c4ed4679f311a9bd88a134a25b18310b1c43ef14
Dominik Martynski <74902424+20094523@users.noreply.github.com>
2024-08-15 18:04:13 +0100

    mongo lab exercises done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -3,8 +3,16 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true }
 });
+
+// Password validation function
+const passwordValidator = (password) => {
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return regex.test(password);
+}
+
+UserSchema.path('password').validate(passwordValidator, 'Password should be atleast 8 characters etc.');
 
 export default mongoose.model('User', UserSchema);
