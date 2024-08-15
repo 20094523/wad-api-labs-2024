85a49225229364e1027d9163ae538c3d50d5b810
Dominik Martynski <74902424+20094523@users.noreply.github.com>
2024-08-15 21:24:07 +0100

    sorted out problem with lab. everything works now.

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,20 +3,23 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
-import defaultErrHandler from './errHandler'
+import defaultErrHandler from './errHandler';
 import authenticate from './authenticate';
-import moviesRouter from './api/movies';   //import movies router
+import moviesRouter from './api/movies';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT; 
+const port = process.env.PORT;
 
-app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
-app.use('/api/movies',authenticate,  moviesRouter);
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',  
+}));
+
 app.use(express.json());
+app.use('/api/movies', authenticate, moviesRouter); 
 app.use('/api/users', usersRouter);
+
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
